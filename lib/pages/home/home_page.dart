075417296c1193e8b04165f58e92bdd6308510b1
Evan <suzukaze.hazuki2020@gmail.com>
2023-09-29 10:28:06 +0900

    update title color

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -9,7 +9,12 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.teal,
         appBar: AppBar(
           backgroundColor: Colors.teal,
-          title: const Text('MI Card'),
+          title: const Text(
+            'MI Card',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
         ),
         body: Center(
           child: Column(
@@ -36,7 +41,7 @@ class HomePage extends StatelessWidget {
                   color: Colors.teal.shade100,
                 ),
               ),
-             SizedBox(
+              SizedBox(
                 height: 20,
                 width: 150,
                 child: Divider(
@@ -47,38 +52,37 @@ class HomePage extends StatelessWidget {
                 color: Colors.white,
                 margin:
                     const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child:  ListTile(
-                      leading: const Icon(
-                        Icons.phone,
-                        color: Colors.teal,
+                child: ListTile(
+                    leading: const Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+86 123 456 789',
+                      style: TextStyle(
+                        fontFamily: 'SourceSan3',
+                        fontSize: 18,
+                        color: Colors.teal.shade900,
                       ),
-                      title: Text(
-                        '+86 123 456 789',
-                        style: TextStyle(
-                          fontFamily: 'SourceSan3',
-                          fontSize: 18,
-                          color: Colors.teal.shade900,
-                        ),
-                      )),
+                    )),
               ),
               Card(
                 color: Colors.white,
                 margin:
                     const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
                 child: ListTile(
-
-                      leading: const Icon(
-                        Icons.email,
-                        color: Colors.teal,
+                    leading: const Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '123456@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'SourceSan3',
+                        fontSize: 18,
+                        color: Colors.teal.shade900,
                       ),
-                      title: Text(
-                        '123456@gmail.com',
-                        style: TextStyle(
-                          fontFamily: 'SourceSan3',
-                          fontSize: 18,
-                          color: Colors.teal.shade900,
-                        ),
-                      )),
+                    )),
               ),
             ],
           ),
