a65b7cc079e916b459e92495ae080058ddc77306
Evan <suzukaze.hazuki2020@gmail.com>
2023-10-08 12:13:56 +0900

    版式

--- lib/pages/examples/example_typography.dart
+++ lib/pages/examples/example_typography.dart
@@ -1,4 +1,6 @@
+import 'package:bilibili/utils/yldm/toast_util.dart';
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
 
 class ExampleTypographyPage extends StatefulWidget {
   const ExampleTypographyPage({super.key});
@@ -10,6 +12,103 @@ class ExampleTypographyPage extends StatefulWidget {
 class _ExampleTypographyPageState extends State<ExampleTypographyPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView(
+      children: [
+        container("基本版式"),
+        const GFTypography(
+          text: 'GF Header Typo1',
+          type: GFTypographyType.typo1,
+          dividerColor: Colors.transparent,
+        ),
+        const GFTypography(
+          text: 'GF Header Typo2',
+          type: GFTypographyType.typo2,
+          dividerColor: Colors.transparent,
+        ),
+        const GFTypography(
+          text: 'GF Header Typo3',
+          type: GFTypographyType.typo3,
+          dividerColor: Colors.transparent,
+        ),
+        const GFTypography(
+          text: 'GF Header Typo4',
+          type: GFTypographyType.typo4,
+          dividerColor: Colors.transparent,
+        ),
+        const GFTypography(
+          text: 'GF Header Typo5',
+          type: GFTypographyType.typo5,
+          dividerColor: Colors.transparent,
+        ),
+        const GFTypography(
+          text: 'GF Header Typo6',
+          type: GFTypographyType.typo6,
+          dividerColor: Colors.transparent,
+        ),
+        container("带图标"),
+        const GFTypography(
+          text: 'GF Header Typo1',
+          type: GFTypographyType.typo1,
+          dividerColor: Colors.transparent,
+          icon: Icon(Icons.send),
+        ),
+        const GFTypography(
+          text: 'GF Header Typo2',
+          type: GFTypographyType.typo2,
+          dividerColor: Colors.transparent,
+          icon: Icon(Icons.send),
+        ),
+        const GFTypography(
+          text: 'GF Header Typo3',
+          type: GFTypographyType.typo3,
+          dividerColor: Colors.transparent,
+          icon: Icon(Icons.send),
+        ),
+        const GFTypography(
+          text: 'GF Header Typo4',
+          type: GFTypographyType.typo4,
+          dividerColor: Colors.transparent,
+          icon: Icon(Icons.send),
+        ),
+        const GFTypography(
+          text: 'GF Header Typo5',
+          type: GFTypographyType.typo5,
+          dividerColor: Colors.transparent,
+          icon: Icon(Icons.send),
+        ),
+        const GFTypography(
+          text: 'GF Header Typo6',
+          type: GFTypographyType.typo6,
+          dividerColor: Colors.transparent,
+          icon: Icon(Icons.send),
+        ),
+        container("带背景图"),
+        const GFTypography(
+          text: 'GF Header Typo1',
+          type: GFTypographyType.typo1,
+          icon: Icon(Icons.send),
+          dividerColor: Colors.transparent,
+          backgroundImage: NetworkImage(
+              'https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_960_720.jpg'),
+        ),
+      ],
+    );
+  }
+
+  toast(content) {
+    ToastUtil.centerToast(content, context);
+  }
+
+  container(String title) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+      alignment: Alignment.centerLeft,
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 20,
+        ),
+      ),
+    );
   }
 }
